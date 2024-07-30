bd57f3509fdba1e6e4b88186c34d6c42a1fd38d0
suhyeoonn <suhyeon1740@gmail.com>
2024-07-30 16:09:13 +0900

    ♻️ 롬복으로 Member 코드 리팩토링

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
     @Autowired
@@ -20,10 +22,10 @@ public class MemberController {
 
     @PostMapping("/signup")
     public String createMember(MemberForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
         Member member = form.toEntity();
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
