ff29ac6c75b488cd31d562a47a9e72b09d90de6d
suhyeoonn <suhyeon1740@gmail.com>
2024-08-01 22:40:32 +0900

    ✨ 셀프체크 회원 조회 기능 구현

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -17,7 +21,7 @@ public class MemberController {
 
     @GetMapping("/signup")
     public String signUp() {
-        return "member/signup";
+        return "members/signup";
     }
 
     @PostMapping("/signup")
@@ -27,5 +31,19 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        Iterable<Member> memberList = memberRepository.findAll();
+        model.addAttribute(memberList);
+        return "members/index";
     }
 }
