3962eaf4726ab6975965c07bc4c526b12bbe53ac
suhyeoonn <suhyeon1740@gmail.com>
2024-08-01 22:30:37 +0900

    ✨ 게시글 읽기 기능 구현

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -29,4 +33,21 @@ public class ArticleController {
         return "";
     }
 
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id =" +id);
+        Article articleEntity = articleRepository.findById(id).orElse(null); // id 값이 없으면 null 반환
+        model.addAttribute("article", articleEntity);
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 넓은 범위로 해석 -> 업캐스팅
+        // 좁은 범위로 해석 -> 다운캐스팅
+        // 가장 상위 인터페이스 Iterable을 List로 형변환 -> 다운캐스팅
+        List<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleList", articleEntityList);
+        return "articles/index";
+    }
 }
